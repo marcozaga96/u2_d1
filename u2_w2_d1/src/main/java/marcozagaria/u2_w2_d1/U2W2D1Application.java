16d0e4e106b1f7baa8827541a1592bb60267283a
Marco Zagaria <marcozagaria@live.com>
2024-10-29 14:49:17 +0100

    inizio

--- u2_w2_d1/src/main/java/marcozagaria/u2_w2_d1/U2W2D1Application.java
+++ u2_w2_d1/src/main/java/marcozagaria/u2_w2_d1/U2W2D1Application.java
@@ -1,13 +1,13 @@
-package marcozagaria.w2_d1;
+package marcozagaria.u2_w2_d1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class W2D1Application {
+public class U2W2D1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(W2D1Application.class, args);
+		SpringApplication.run(U2W2D1Application.class, args);
 	}
 
 }
--- u2_w2_d1/src/test/java/marcozagaria/u2_w2_d1/U2W2D1ApplicationTests.java
+++ u2_w2_d1/src/test/java/marcozagaria/u2_w2_d1/U2W2D1ApplicationTests.java
@@ -1,10 +1,10 @@
-package marcozagaria.w2_d1;
+package marcozagaria.u2_w2_d1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class W2D1ApplicationTests {
+class U2W2D1ApplicationTests {
 
 	@Test
 	void contextLoads() {
