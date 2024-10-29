9ba1b5095aa1fe1e2964f41a5e411d66139e4e8e
Marco Zagaria <marcozagaria@live.com>
2024-10-29 14:22:32 +0100

    inizio

---
+++ w2_d1/src/test/java/marcozagaria/w2_d1/W2D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package marcozagaria.w2_d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
